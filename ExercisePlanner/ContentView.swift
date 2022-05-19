3ac4e77c43ff7f06b2620d40b8c9478b56c9505b
Reshma Unnikrishnan <ruvlmoon@gmail.com>
2022-05-19 12:59:00 +0200

    Added App to the project

---
+++ ExercisePlanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExercisePlanner
+//
+//  Created by Reshma Unnikrishnan on 19.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
