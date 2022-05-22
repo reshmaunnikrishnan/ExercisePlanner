15c251d4c7088275910a464cf7019199854a97a2
Reshma Unnikrishnan <ruvlmoon@gmail.com>
2022-05-23 00:15:20 +0200

    Added some assets, minor changes in the  group classification of classes

--- ExercisePlanner/ExerciseList/ExerciseListViewModel.swift
+++ ExercisePlanner/ExerciseList/ExerciseListViewModel.swift
@@ -8,14 +8,11 @@
 import Foundation
 import Combine
 
-protocol ExerciseListViewModelDelegate: AnyObject {
-    func showResults() async
-}
+
 
 class ExerciseListViewModel: ObservableObject  {
     
     // MARK: - Properties
-    weak var delegate: ExerciseListViewModelDelegate?
     private let service: ExercisePlannerProvider = ExercisePlannerService(session: .shared)
     
     private var disposables = Set<AnyCancellable>()
@@ -34,23 +31,5 @@ class ExerciseListViewModel: ObservableObject  {
                 self.exerciseList = response.results
             }.store(in: &disposables)
     }
-    
 }
-    
 
-//         private func bind() {
-//             $exerciseList.map({ $0?.name }).assign(to: &$name)
-//             $exerciseList.map({ $0.description }).assign(to: &$description)
-//             $exerciseList.map({ $0.images }).assign(to: &$images)
-//             $exerciseList.map({ $0.images.first }).assign(to: &$selectedImage)
-//         }
-//
-         
-//
-//        @Published var name: String?
-//        @Published var description: String?
-//        @Published var images: [Images]?
-//        @Published var selectedImage: Images?
-//
-//
- 
---
+++ ExercisePlanner/VariationDetail/VariationsDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  VariationsCard.swift
+//  ExercisePlanner
+//
+//  Created by Reshma Unnikrishnan on 21.05.22.
+//
+
+import SwiftUI
+
+struct VariationsCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VariationsCard_Previews: PreviewProvider {
+    static var previews: some View {
+        VariationsCard()
+    }
+}
